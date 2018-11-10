4b4cd93efdf290a100d3076331f5dd9eacc02f58
Naeem Ally <naeem@fingerprintsoft.org>
2018-11-10 12:29:13 +0200

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,57 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  constructor(private formBuilder: FormBuilder) {
+    this.form1 = this.formBuilder.group({
+      'id_number' : [null, Validators.required]
+    });
+
+    this.form2 = this.formBuilder.group({
+      'dob' : [null, Validators.required],
+      'surname' : [null, Validators.required],
+      'gender' : [null, Validators.required]
+    });
+  }
+
+  form1: FormGroup;
+  form2: FormGroup;
+  results: any = {start: false};
+
+  logForm1() {
+    this.form2.reset();
+    console.log("Form 1");
+    console.log(this.form1.value);
+    if (this.form1.value.id_number == "12345") {
+      this.results = {
+        start: true,
+        found: true,
+        organisations: ["abc", "def"]
+      }
+    } else {
+      this.results = {
+        start: true,
+        found: false,
+        organisations: []
+      }
+    }
+  }
+
+  logForm2() {
+    this.form1.reset();
+    console.log("Form 2");
+    console.log(this.form2.value);
+    if (this.form2.value.surname.toLowerCase() == "ally") {
+      this.results = {
+        start: true,
+        found: true,
+        organisations: ["abc", "def"]
+      }
+    } else {
+      this.results = {
+        start: true,
+        found: false,
+        organisations: []
+      }
+    }
+  }
 }
